3a7b7378a08c6d9bef00de786ada4ffb722e5e29
TUMMY2022 <to88my@gmail.com>
2023-02-26 00:00:09 -0500

    updating the code

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require("net");
-
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541// PORT number here,
+    host: IP, // IP address here,
+    port: PORT// PORT number here,
   });
 
  // interpret incoming data as text
@@ -14,6 +14,10 @@ const connect = function () {
     console.log("Successfully connected to game server");
     conn.write("Name: THT");
     conn.write("Say: let us have fun with the snake");
+    console.log("Move up=>w"
+    +" Move down=>s" +
+    " Move left=>a" +
+    " Move right=>d");
     //setInterval(() => { 
       //conn.write("Move: up")},100);
     })
