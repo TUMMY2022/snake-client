367b277d66c02023b98e621bc6c9f51080d6507a
TUMMY2022 <to88my@gmail.com>
2023-02-25 20:00:20 -0500

    updating the code

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
-const {connect}= require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("Connecting ...");
 connect();
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-  stdin.on('data', (key) => {
-    process.stdout.write('.');
-  });  
-};
 
 setupInput();
